ea68b968a386618fc1329c256cc7e83a70f2afb5
Roman Hutsoliak <roman.hutsoliak@gmail.com>
2023-01-19 20:03:38 +0200

    Firebase for the capacitor

--- app/Console/Commands/TaskExecution.php
+++ app/Console/Commands/TaskExecution.php
@@ -4,10 +4,10 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Models\Task;
+use Illuminate\Support\Facades\Http;
 
 class TaskExecution extends Command
 {
-    use \App\Traits\ApiRequest;
 
     /**
      * The name and signature of the console command.
@@ -45,18 +45,29 @@ class TaskExecution extends Command
                 if (!$userDevice->notificationToken)
                     continue;
 
-                $this->sendPost('https://exp.host/--/api/v2/push/send', [
-                    'to' => $userDevice->notificationToken,
-                    'sound' => 'default',
-                    'title' => $task->name,
-                    'body' => $task->description,
+                Http::withHeaders([
+                    'Accept' => 'application/json',
+                    'Content-Type' => 'application/json',
+                    'Authorization' => 'key='.env('FIRE_BASE_KEY')
+                ])->post('https://fcm.googleapis.com/fcm/send', [
+                    'notification' => [
+                        'title' => trim($task->name),
+                        'body' => trim($task->description),
+                        'badge' => '1',
+                        'sound' => 'default',
+                        'showWhenInForeground' => true,
+                    ],
+                    'content_available' => false,
                     'data' => [
                         'redirectTo' => '/tasks/' . $task->id,
                     ],
-                ], [
-                    'Accept: application/json',
-                    'Accept-encoding: gzip, deflate',
-                    'Content-Type: application/json',
+                    'android' => [
+                        'icon' => 'firebase_icon'
+                    ],
+                    'priority' => 'High',
+                    'registration_ids' => [
+                        $userDevice->notificationToken
+                    ],
                 ]);
             }
             if (!$task->mustBeCompleted && $task->periodType != 5) {
