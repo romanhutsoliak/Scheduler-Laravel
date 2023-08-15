c134aaae2ccc09b0ebced66de070b35e2b75fbbe
Roman Hutsoliak <roman@zapiet.com>
2023-08-15 19:40:45 +0300

    Task category warnings

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use App\Enums\TaskPeriodTypesEnum;
 use App\Managers\TaskManager;
-use App\Services\CalculateNextRunDateTimeService;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
